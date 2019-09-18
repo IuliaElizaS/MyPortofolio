dd24662c96c1b3db3193beaba680371d20ace6e2
Iulia <iuliaelizamts@gmail.com>
2019-09-18 11:37:53 +0200

    updated Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,10 +10,24 @@ class Contact extends React.Component {
         <Menu/>
         <div className="contact">
           <p className="content">
-            I'm opened for questions, sugestions, new oportunities.
-            Please feel free to contact me using the form below or by Linkedin
+            I am opened for questions, sugestions, new oportunities.
+            Please feel free to contact me by<a href="https://de.linkedin.com/in/iuliaelizasabadas">Linkedin</a>or use the form below
           </p>
-          <form className="contactForm"></form>
+          <form className="contactForm" name="contact" method="post">
+            <input type="hidden" name="form-name" value="portofolioContactForm" />
+            <p className="formRubric">
+              <label>Your Name: <input type="text" name="name"/></label>
+            </p>
+            <p className="formRubric">
+              <label>Your Email: <input type="email" name="email"/></label>
+            </p>
+            <p className="formRubric">
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <button type="submit">Send message</button>
+          </form>
+          {/* Netlify's contact form handling implementation from https://www.netlify.com/docs/form-handling/
+            and https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/ */}
         </div>
       </div>
     )
