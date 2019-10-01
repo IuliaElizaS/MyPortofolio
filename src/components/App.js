7cd9950c47184078328bbfa6dd09378c504c71dd
Iulia <iuliaelizamts@gmail.com>
2019-10-01 11:28:28 +0200

    aded Helmet component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../style/App.css';
 import '../style/responsive.css';
+import {Helmet} from "react-helmet";
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -9,9 +10,16 @@ class App extends React.Component {
   render (){
     return (
       <div className="app">
+        <Helmet>
+          <meta name="description" content="junior front-end developer Iulia Eliza Sabadas's portofolio " ></meta>
+          <meta name="keywords" content="front end developer, portofolio, Iulia Eliza Sabadas"></meta>
+          <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Josefin+Sans|Marck+Script&display=swap" rel="stylesheet"></link>
+          <title>IuliaElizaS' Portofolio</title>
+        {/* Helmet component and it's implementation from https://github.com/nfl/react-helmet*/}
+        </Helmet>
         <Header/>
         <Main/>
-        <Footer/>        
+        <Footer/>
       </div>
     )
   }
